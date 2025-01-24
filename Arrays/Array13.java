4b8520c648b8546729b517ec2f3a27c52d6b7841
MohitKumar <K2004mohit@gmail.com>
2025-01-24 21:21:35 +0530

    Missing Number

---
+++ Arrays/Array13.java
@@ -0,0 +1,19 @@
+class Array13 {
+    // Function to find the smallest positive number missing from the array.
+    public int missingNumber(int[] arr) {
+        // Your code here
+        int n=arr.length;
+        boolean vis[]=new boolean[n];
+        for(int i=0; i<n; i++){
+            if(arr[i]>0&&arr[i]<=n){
+                vis[arr[i]-1]=true;
+            }
+        }
+        for(int i=1; i<=n; i++){
+            if(!vis[i-1]){
+                return i;
+            }
+        }
+        return n+1;
+    }
+}
